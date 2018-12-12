7d2cb9ca61529fe1e0a9743e59dc18ca9504aa0b
Sanni <prasadsunny1@gmail.com>
2018-12-12 12:35:34 +0530

    Update appcenter-pre-build.sh

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -3,12 +3,6 @@
 # For Xamarin Android or iOS, change the version name located in AndroidManifest.xml and Info.plist. 
 # AN IMPORTANT THING: YOU NEED DECLARE VERSION_NAME ENVIRONMENT VARIABLE IN APP CENTER BUILD CONFIGURATION.
 
-$LASTBUILD=$(date +"%Y%m%d")
-$TODAYBUILD=1
-$VERSION_NAME=$LASTBUILD
-echo $LASTBUILD
-echo $VERSION_NAME
-echo $TODAYBUILD
 if [ ! -n "$VERSION_NAME" ]
 then
     echo "You need define the VERSION_NAME variable in App Center"
@@ -17,10 +11,7 @@ fi
 
 ANDROID_MANIFEST_FILE=$APPCENTER_SOURCE_DIRECTORY/AppCenterTest.Android/Properties/AndroidManifest.xml
 INFO_PLIST_FILE=$APPCENTER_SOURCE_DIRECTORY/AppCenterTest.iOS/Info.plist
-echo "AAAA"
-echo $APPCENTER_SOURCE_DIRECTORY
-echo $ANDROID_MANIFEST_FILE
-echo $INFO_PLIST_FILE
+
 if [ -e "$ANDROID_MANIFEST_FILE" ]
 then
     echo "Updating version name to $VERSION_NAME in AndroidManifest.xml"
